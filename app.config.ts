f38b0ba47f4c42b79e342101afdd31b550f58cb0
Kacper Kuchta <kacper.kuchta@here.com>
2025-03-28 16:25:49 +0100

    github  actions

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   ssr: true,
   server: {
+    baseURL: "./",
     preset: "github_pages",
     prerender: {
       crawlLinks: true,
